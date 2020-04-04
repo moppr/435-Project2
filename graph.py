9e399e25c06017c40e902676b5d7d46d7bbc24b3
moppr <mantony42@gmail.com>
2020-04-04 01:23:06 -0400

    Create implementation for Graph & Node classes.

---
+++ graph.py
@@ -0,0 +1,37 @@
+from node import Node
+
+
+class Graph:
+    """Undirected unweighted graph implementation using an adjacency list."""
+
+    def __init__(self):
+        """Init with blank adjacency list as dictionary."""
+        self.adjacency_list = {}
+
+    def add_node(self, value):
+        """Create new node with specified value and add to adjacency list."""
+        self.adjacency_list[Node(value)] = set()
+
+    def add_undirected_edge(self, first, second):
+        """Create adjacency between nodes first and second, provided they exist."""
+        self._check_node_validity((first, second))
+
+        self.adjacency_list[first].add(second)
+        self.adjacency_list[second].add(first)
+
+    def remove_undirected_edge(self, first, second):
+        """Remove adjacency between nodes first and second, provided they exist."""
+        self._check_node_validity((first, second))
+
+        self.adjacency_list[first].remove(second)
+        self.adjacency_list[second].remove(first)
+
+    def get_all_nodes(self):
+        """Return set containing all nodes present in adjacency list."""
+        return set(self.adjacency_list)
+
+    def _check_node_validity(self, nodes):
+        """Raise error if any node in nodes does not exist in the adjacency list."""
+        for node in nodes:
+            if node not in self.adjacency_list:
+                raise ValueError(f"Node {node} does not currently exist in the graph")
